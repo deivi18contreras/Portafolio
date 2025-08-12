0b4b30374b8f4f969a0a96e18fad939f09ba8fc4
Deivi Contreras <deivibogoya@gmail.com>
2025-08-12 15:58:27 -0500

    Ya esta hecho

--- Restaurante/mesa.js
+++ Restaurante/mesa.js
@@ -1,43 +1,8 @@
-let registroMesa = [];
+function gurdar () {
+  const nombre = document.getElementById("nombre").value.trim();
+  const apellidos = document.getElementById("apellidos").value.trim();
+  const documento = document.getElementById("documento").value.trim();
+  const correo = document.getElementById("correo").value.trim();
 
-function guardar() {
-  const numero = document.getElementById("numero").value.trim();
-  const capMesa = document.getElementById("capaMesa").value.trim();
-  const lugar = document.getElementById("lugarMesa").value.trim();
-  if (!numero) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "No puedes pasar sin asignar un número de mesa!",
-    });
-  } else if (!capMesa) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "No puedes pasar sin registrar la capacidad de personas!",
-    });
-  } else if (!lugar) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "No puedes pasar sin asignar el lugar de la mesa!",
-    });
-  } else {
-    let reg = { numero, capMesa, lugar };
-    datos.push(reg);
-    console.log(datos);
-    pintarDatos();
-
-    document.getElementById("numero").value = "";
-    document.getElementById("capaMesa").value = "";
-    document.getElementById("lugarMesa").value = "";
-    
-
- 
-    let modalElement = document.getElementById("miModal");
-    let modal = bootstrap.Modal.getInstance(modalElement);
-    if (modal) {
-      modal.hide();
-    }
-  }
+  
 }
