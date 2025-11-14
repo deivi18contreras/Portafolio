769b3ec9670473fdd0e6cdc53a937a7a27d906ac
deivi18contreras <deivibogoya@gmail.com>
2025-11-14 17:51:25 -0500

    Delete JUEGO directory

--- JUEGO/modos.js
+++
@@ -1,5 +0,0 @@
- document.getElementById("config-btn").addEventListener("click", function (e) {
-    e.preventDefault();
-    const menuConfig = document.querySelector(".menu_config");
-    menuConfig.classList.toggle("active");
-  });
