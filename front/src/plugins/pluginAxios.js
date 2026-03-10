9aa4b548feffa6851c3aac842bc3e3446f0ecebb
deivi18contreras <deivibogoya@gmail.com>
2026-03-09 22:19:03 -0500

    fix axios baseURL

--- front/src/plugins/pluginAxios.js
+++ front/src/plugins/pluginAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/Auth.js";
 
 const axiosInstance = axios.create({
-  baseURL: "https://adventurous-mindfulness-production-4a69.up.railway.app/api/login",
+  baseURL: "https://adventurous-mindfulness-production-4a69.up.railway.app/api",
   headers: {
     "Content-Type": "application/json"
   }
