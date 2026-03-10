080b5e95cf4fcb10089e9af0b6a1aa81efd1b2ed
deivi18contreras <deivibogoya@gmail.com>
2026-03-09 22:00:37 -0500

    Fix backend url

--- front/src/plugins/pluginAxios.js
+++ front/src/plugins/pluginAxios.js
@@ -1,24 +1,22 @@
 import axios from 'axios';
-import { useAuthStore } from '../store/Auth.js'; // Asegúrate de que este es el nombre de tu store
+import { useAuthStore } from '../store/Auth.js';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  //baseURL: 'https://backend-production-4304.up.railway.app/api',
+  baseURL: 'https://backend-production-4304.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
-// Interceptor para añadir el header x-token en cada petición
 axiosInstance.interceptors.request.use(
   (config) => {
     const authStore = useAuthStore();
     const token = authStore.token;
 
     if (token) {
-      // Configuramos el header personalizado solicitado
       config.headers['x-token'] = token;
     }
+
     return config;
   },
   (error) => {
